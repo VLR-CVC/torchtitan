8152ab0c0300e802a174561828468ab4f60c4254
tomiock <ockier1@gmail.com>
2025-09-30 17:19:51 +0000

    removed prints from train file

--- torchtitan/train.py
+++ torchtitan/train.py
@@ -158,7 +158,6 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
         # META init
         with torch.device("meta"):
             model = self.train_spec.model_cls(model_args)
-            print(model)
 
         # Build the collection of model converters. No-op if `model.converters` empty
         model_converters = build_model_converters(job_config, parallel_dims)
@@ -403,9 +402,6 @@ class Trainer(torch.distributed.checkpoint.stateful.Stateful):
                 if isinstance(v, torch.Tensor):
                     input_dict[k] = v.to(device_type)
             labels = labels.to(device_type)
-
-            for key, value in input_dict.items():
-                print(key, value.shape)
 
             yield input_dict, labels
 
